438b0c232ed828eeff8912249963e4aa717f7036
AliMaaz <malik.alimaax@gmail.com>
2025-02-12 22:38:36 +0500

    DSD

--- Lab3/RTL/lab3task.sv
+++ Lab3/RTL/lab3task.sv
@@ -1,13 +1,14 @@
-module lab3task(input  logic a, 
+module lab3task(output logic x,
+                output logic y,
+                input  logic a, 
                 input  logic b,
-                input  logic c,
-                output logic x,
-                output logic y
+                input  logic c
                 );  
     logic or_out, nand_out, xor_out;                
 
-    
-    assign xor_out  = nand ^ or; // =>XOR gate 
+    assign or_out   = a | b;             // =>OR gate
+    assign nand_out = ~(a & b);          // =>NAND gate
+    assign xor_out  = nand_out ^ or_out; // =>XOR gate 
     assign x        = (~c) ^ or_out;     // =>XOR operation with inverted c
     assign y        = xor_out & or_out;  // =>AND operation   
     
